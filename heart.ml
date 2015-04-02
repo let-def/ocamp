90b3e88f9f30fa3f158c258def6952902feb8827
Frederic Bour <frederic.bour@lakaban.net>
2015-04-02 12:51:03 +0200

    Add CC-0 LICENSE

--- heart.ml
+++ heart.ml
@@ -1,3 +1,20 @@
+(*
+  OCamp by Frédéric Bour <frederic.bour(_)lakaban.net>
+  To the extent possible under law, the person who associated CC0 with
+  OCamp has waived all copyright and related or neighboring rights to OCamp.
+
+  You should have received a copy of the CC0 legalcode along with this
+  work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+  Website: https://github.com/def-lkb/ocamp
+
+  Version 0.1, April 2015
+
+  --
+
+  The concept of 'Heart' is inspired from Jenga,
+  https://github.com/janestreet/jenga
+*)
 type t = { mutable deps: t list; mutable broken: bool }
 
 let fresh () = { deps = []; broken = false }
--- ocamp.ml
+++ ocamp.ml
@@ -1,3 +1,15 @@
+(*
+  OCamp by Frédéric Bour <frederic.bour(_)lakaban.net>
+  To the extent possible under law, the person who associated CC0 with
+  OCamp has waived all copyright and related or neighboring rights to OCamp.
+
+  You should have received a copy of the CC0 legalcode along with this
+  work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+  Website: https://github.com/def-lkb/ocamp
+
+  Version 0.1, April 2015
+*)
 open Utils
 
 let main () =
